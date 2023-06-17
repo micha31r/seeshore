9289d4f2cc66a41a797343a6c22ea9ef4f283445
Michael Ren <michaelren0129@gmail.com>
2023-06-17 23:59:01 +1000

    Add create view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AccountView from '../views/AccountView.vue'
+import CreateView from '../views/CreateView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/account',
       name: 'account',
       component: AccountView
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: CreateView
     }
   ]
 })
---
+++ src/views/CreateView.vue
@@ -0,0 +1,56 @@
+<template>
+  <Protect>
+    <div class='grid'>
+      <Navbar pageName='Create' />
+
+      <div class='editor'>
+        <p class='help-text'>Tap to add media. Hold to add text.</p>
+        <div class='image'></div>
+        <SolidButton class='send'>Send</SolidButton>
+      </div>
+    </div>
+  </Protect>
+</template>
+
+<script setup>
+import Navbar from '../components/Navbar.vue'
+</script>
+
+<style scoped lang='scss'>
+@import '../assets/themes';
+
+.grid {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  flex: 1;
+}
+
+.editor {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  width: max-content;
+  margin: auto;
+
+  .help-text {
+    color: $color-text-2;
+    margin: 0;
+    text-align: center;
+  }
+
+  .image {
+    display: block;
+    width: 400px;
+    height: 560px;
+    border-radius: 15px;
+    background: $color-bg-2;
+    margin: 0 auto;
+  }
+
+  .send {
+    border-radius: 100px;
+    padding: 10px 15px;
+    margin: 0 auto;
+  }
+}
+</style>
