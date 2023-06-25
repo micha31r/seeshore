c5a307e240d9008dea42ac6a80e3ba5c7e1a6193
Michael Ren <michaelren0129@gmail.com>
2023-06-26 09:37:12 +1000

    Clean up HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,85 @@
 <template>
   <div class='grid'>
     <Navbar pageName='Stories' />
-    <Feed />
+
+    <div class='feed'>
+      <Story v-for='data in groups' :data='data' />
+    </div>
   </div>
 </template>
 
 <script setup>
+import { supabase } from '../supabase'
+import { ref, onMounted } from 'vue'
+import store from '../store'
 import Navbar from '../components/Navbar.vue'
-import Feed from '../components/Feed.vue'
+import Story from '../components/Story.vue'
+
+const stories = ref([])
+const groups = ref([])
+
+onMounted(async () => {
+  stories.value = await getStories()
+  groups.value = groupStories(stories)
+})
+
+async function getStories () {
+  try {
+    const { data, error } = await supabase
+      .from('sharing')
+      .select(`
+        profile,
+        story (
+          id,
+          profile (id, avatar_url, full_name),
+          media_url
+        )
+      `)
+      .eq('profile', store.profile.id)
+    
+    if (error) throw error
+
+    return data.map(item => item.story)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function groupStories(stories) {
+  let groups = {}
+
+  stories.value.forEach(item => {
+    const key = item.profile.id
+
+    if (!(key in groups)) {
+      groups[key] = []
+    }
+
+    groups[key].push(item)
+  })
+
+  return groups
+}
 </script>
 
 <style scoped lang='scss'>
+$nav-height: calc(1.1em + 15px + 15px * 2);
+
 .grid {
   display: grid;
   grid-template-rows: auto 1fr;
   flex: 1;
+}
+
+.feed {
+  display: grid;
+  gap: 15px;
+  padding: 0 15px 15px;
+  margin: 0 auto;
+
+  .story {
+    width: 500px;
+    height: 700px;
+  }
 }
 </style>
