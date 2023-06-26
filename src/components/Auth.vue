f55dcdb9974d8ca2804d6b1035f895f0c1ac6315
Michael Ren <michaelren0129@gmail.com>
2023-06-27 02:32:06 +1000

    Update Auth.vue

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,20 +1,22 @@
 <template>
-  <form @submit.prevent>
-    <h1>Log In</h1>
+  <div class='wrapper'>
+    <form @submit.prevent>
+      <h1>Log In</h1>
 
-    <div>
-      <label>Email</label>
-      <IconInput type='email' v-model='email' placeholder='you@example.com' icon='mail' required/>
-    </div>
+      <div>
+        <label>Email</label>
+        <IconInput type='email' v-model='email' placeholder='you@example.com' icon='mail' required/>
+      </div>
 
-    <SolidButton class='submit solid' @click='OTPLogin' :disabled='OTPState != 0'>
-      <template v-if='OTPState == 0'>Get Magic Link</template>
-      <template v-else-if='OTPState == 1'>Sending...</template>
-      <template v-else>Check Your Inbox</template>
-    </SolidButton>
-    
-    <OutlineButton class='oauth outline' @click.submit.prevent='GoogleLogin'>Continue with Google</OutlineButton>
-  </form>
+      <SolidButton class='submit solid' @click='OTPLogin' :disabled='OTPState != 0'>
+        <template v-if='OTPState == 0'>Get Magic Link</template>
+        <template v-else-if='OTPState == 1'>Sending...</template>
+        <template v-else>Check Your Inbox</template>
+      </SolidButton>
+      
+      <OutlineButton class='oauth outline' @click.submit.prevent='GoogleLogin'>Continue with Google</OutlineButton>
+    </form>
+  </div>
 </template>
 
 <script setup>
@@ -61,11 +63,23 @@ async function GoogleLogin () {
 @import '../assets/themes';
 
 @include use-theme {
+.wrapper {
+  display: flex;
+  width: 100%;
+  height: 100vh;
+}
+
 form {
   display: grid;
   gap: 10px;
   width: 400px;
+  max-width: 100%;
   margin: auto;
+  padding: 15px;
+
+  @media (max-width: 500px) {
+    padding: 10px;
+  }
 
   h1 {
     margin: 0 0 20px;
